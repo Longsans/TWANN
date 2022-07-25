931fd52d2e92af9f2b9005eecf52d8a61d7c327d
Long <gameofnoobs17@gmail.com>
2022-07-25 10:22:13 +0700

    form validation with react-hook-form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { phoneFormat } from "../regex";
+import "./ContactForm.css";
 
 export const ContactForm = ({
   contact,
@@ -9,62 +12,115 @@ export const ContactForm = ({
   onAddressChange,
   setUpdating,
 }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    console.log(errors.phone);
+  }, [errors]);
+
   return (
-    <form
-      className="d-flex flex-grow-1 align-items-center"
-      onSubmit={(e) => {
-        e.preventDefault();
-        onSubmit();
-      }}
-    >
-      <div className="me-5">
-        <label htmlFor="phone" className="d-block mb-1">
-          Phone
-        </label>
-        <input
-          type="text"
-          id="phone"
-          value={contact.phone}
-          onChange={onPhoneChange}
-          disabled={!updating}
-          className="border border-1 shadow-sm"
-        />
-      </div>
-      <div className="w-2"></div>
-      <div>
-        <label htmlFor="address" className="d-block mb-1">
-          Address
-        </label>
-        <input
-          type="text"
-          id="address"
-          value={contact.address}
-          onChange={onAddressChange}
-          disabled={!updating}
-          className="border border-1 shadow-sm"
-        />
-      </div>
-      {updating ? (
-        <div className="d-flex align-items-end ms-5 align-self-end">
-          <input type="submit" value="Save" className="btn btn-success me-2" />
-          <button
-            className="btn btn-secondary"
-            onClick={() => {
-              setUpdating(false);
-              onCancel();
-            }}
-          >
-            Cancel
-          </button>
+    <form className="flex-grow-1" onSubmit={handleSubmit(onSubmit)}>
+      <div className="row row-cols-4">
+        <div className="col-3 me-5">
+          <label htmlFor="phone" className="d-block mb-1">
+            Phone
+          </label>
         </div>
-      ) : (
-        <button
-          className="btn btn-primary ms-5 align-self-end"
-          onClick={() => setUpdating(true)}
-        >
-          Update
-        </button>
-      )}
+        <div className="col-3">
+          <label htmlFor="address" className="d-block mb-1">
+            Address
+          </label>
+        </div>
+      </div>
+
+      <div className="row row-cols-4">
+        <div className="col-3 me-5">
+          <input
+            type="text"
+            {...register("phone", {
+              required: "Phone number cannot be empty",
+              pattern: {
+                value: phoneFormat,
+                message: "Phone number not in correct format",
+              },
+              minLength: {
+                value: 10,
+                message: "Phone number must be between 10 and 11 characters",
+              },
+              maxLength: {
+                value: 11,
+                message: "Phone number must be between 10 and 11 characters",
+              },
+            })}
+            value={contact.phone}
+            onChange={onPhoneChange}
+            disabled={!updating}
+            className="border border-1 shadow-sm contact-input"
+          />
+        </div>
+        <div className="col-3">
+          <input
+            type="text"
+            {...register("address", {
+              required: "Address cannot be empty",
+              minLength: {
+                value: 1,
+                message: "Address must be between 1 and 75 characters",
+              },
+              maxLength: {
+                value: 75,
+                message: "Address must be between 1 and 75 characters",
+              },
+            })}
+            value={contact.address}
+            onChange={onAddressChange}
+            disabled={!updating}
+            className="border border-1 shadow-sm contact-input"
+          />
+        </div>
+        <div className="col ms-4">
+          {updating ? (
+            <div className="d-flex align-items-end align-self-end">
+              <input
+                type="submit"
+                value="Save"
+                className="btn btn-success me-2"
+              />
+              <button
+                className="btn btn-secondary"
+                onClick={() => {
+                  setUpdating(false);
+                  onCancel();
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="btn btn-primary align-self-end"
+              onClick={() => setUpdating(true)}
+            >
+              Update
+            </button>
+          )}
+        </div>
+      </div>
+
+      <div className="row row-cols-3">
+        <div className="col-3 me-5">
+          <span className="text-danger text-wrap d-block">
+            {errors.phone?.message}
+          </span>
+        </div>
+        <div className="col-3">
+          <span className="text-danger d-block">{errors.address?.message}</span>
+        </div>
+      </div>
     </form>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import "../site.css";
 import { API_URL } from "../config";
 import { useAuth } from "../hooks/useAuth";
-import { ContactForm } from "../components/ContactForm";
+import { useForm } from "react-hook-form";
+import { phoneFormat } from "../regex";
 import { AdPlug } from "../components/AdPlug";
 
 export const Contact = () => {
@@ -13,6 +14,13 @@ export const Contact = () => {
   });
   const [updatingContact, setUpdatingContact] = useState(false);
   const auth = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    getValues,
+  } = useForm();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,7 +33,7 @@ export const Contact = () => {
         if (body) {
           const { phone, address } = body;
           setContact({ phone, address });
-          setEditableContact({ phone, address });
+          reset({ phone, address });
         }
       } catch (errorMsg) {
         alert(errorMsg);
@@ -35,21 +43,13 @@ export const Contact = () => {
   }, []);
 
   const revertChanges = () => {
-    setEditableContact(contact);
+    reset({
+      phone: contact.phone,
+      address: contact.address,
+    });
   };
 
   const handleSaveContact = async () => {
-    if (
-      !(
-        editableContact.phone &&
-        editableContact.phone.trim() &&
-        editableContact.address &&
-        editableContact.address.trim()
-      )
-    ) {
-      alert("You haven't filled all contact info.");
-      return;
-    }
     try {
       let res;
       if (contact) {
@@ -57,7 +57,7 @@ export const Contact = () => {
           `${API_URL}/contact/${auth.user.username}`,
           addHeadersForPostPut({
             method: "PUT",
-            body: JSON.stringify(addUsernameToContact(editableContact)),
+            body: JSON.stringify(addUsernameToContact(getValues())),
           })
         );
       } else {
@@ -65,7 +65,7 @@ export const Contact = () => {
           `${API_URL}/contact/`,
           addHeadersForPostPut({
             method: "POST",
-            body: JSON.stringify(addUsernameToContact(editableContact)),
+            body: JSON.stringify(addUsernameToContact(getValues())),
           })
         );
       }
@@ -84,7 +84,7 @@ export const Contact = () => {
             throw err.username;
         }
       }
-      setContact(editableContact);
+      setContact(getValues());
       alert("Contact updated!");
       setUpdatingContact(false);
     } catch (error) {
@@ -126,19 +126,109 @@ export const Contact = () => {
       <div className="d-flex align-items-center flex-grow-1">
         <h1 className="ms-5">Contact</h1>
         <div className="w-15"></div>
-        <ContactForm
-          contact={editableContact}
-          updating={updatingContact}
-          onSubmit={handleSaveContact}
-          onCancel={revertChanges}
-          onPhoneChange={(e) =>
-            setEditableContact({ ...editableContact, phone: e.target.value })
-          }
-          onAddressChange={(e) =>
-            setEditableContact({ ...editableContact, address: e.target.value })
-          }
-          setUpdating={(val) => setUpdatingContact(val)}
-        />
+        <form
+          className="flex-grow-1"
+          onSubmit={handleSubmit(handleSaveContact)}
+        >
+          <div className="row row-cols-4">
+            <div className="col-3 me-5">
+              <label htmlFor="phone" className="d-block mb-1">
+                Phone
+              </label>
+            </div>
+            <div className="col-3">
+              <label htmlFor="address" className="d-block mb-1">
+                Address
+              </label>
+            </div>
+          </div>
+
+          <div className="row row-cols-4">
+            <div className="col-3 me-5">
+              <input
+                type="text"
+                {...register("phone", {
+                  required: "Phone number cannot be empty",
+                  pattern: {
+                    value: phoneFormat,
+                    message: "Phone number not in correct format",
+                  },
+                  minLength: {
+                    value: 10,
+                    message:
+                      "Phone number must be between 10 and 11 characters",
+                  },
+                  maxLength: {
+                    value: 11,
+                    message:
+                      "Phone number must be between 10 and 11 characters",
+                  },
+                })}
+                disabled={!updatingContact}
+                className="border border-1 shadow-sm contact-input"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="text"
+                {...register("address", {
+                  required: "Address cannot be empty",
+                  minLength: {
+                    value: 1,
+                    message: "Address must be between 1 and 75 characters",
+                  },
+                  maxLength: {
+                    value: 75,
+                    message: "Address must be between 1 and 75 characters",
+                  },
+                })}
+                disabled={!updatingContact}
+                className="border border-1 shadow-sm contact-input"
+              />
+            </div>
+            <div className="col ms-4">
+              {updatingContact ? (
+                <div className="d-flex align-items-end align-self-end">
+                  <input
+                    type="submit"
+                    value="Save"
+                    className="btn btn-success me-2"
+                  />
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => {
+                      setUpdatingContact(false);
+                      revertChanges();
+                    }}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              ) : (
+                <button
+                  className="btn btn-primary align-self-end"
+                  onClick={() => setUpdatingContact(true)}
+                >
+                  Update
+                </button>
+              )}
+            </div>
+          </div>
+
+          <div className="row row-cols-3">
+            <div className="col-3 me-5">
+              <span className="text-danger text-wrap d-block">
+                {errors.phone?.message}
+              </span>
+            </div>
+            <div className="col-3">
+              <span className="text-danger d-block">
+                {errors.address?.message}
+              </span>
+            </div>
+          </div>
+        </form>
+        <div className="w-15"></div>
       </div>
       <AdPlug />
     </div>
